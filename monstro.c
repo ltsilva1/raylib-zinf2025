acc3235a919d555431f2cd4b482e9ee7feb163ff
Panerai <gabrielpanerai@gmail.com>
2025-05-30 15:59:30 -0300

    penis

---
+++ monstro.c
@@ -0,0 +1,5 @@
+//
+// Created by Panerai on 30/05/2025.
+//
+
+#include "monstro.h"
---
+++ monstro.h
@@ -0,0 +1,10 @@
+//
+// Created by Panerai on 30/05/2025.
+//
+
+#ifndef MONSTRO_H
+#define MONSTRO_H
+
+void
+
+#endif //MONSTRO_H
