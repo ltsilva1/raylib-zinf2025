96f256017d448245a368ba9deb134a827d0b6519
ltsilva1 <lucastelles2005@outlook.com>
2025-05-30 16:00:24 -0300

    logica morta

--- logica.c
+++ logica.c
@@ -4,16 +4,6 @@
 
 #include "logica.h"
 
-void processarLogica(Jogo *jogo) {
-    for (int i = 0; i < 16; i++) {
-        for (int j = 0; j < 25; j++) {
-            if (jogo->mapa.mapa[i][j] == 'J') {
-                jogo->jogador.pos.x = i;
-                jogo->jogador.pos.y = j;
-                break;
-            }
-        }
-    }
 
 
 
